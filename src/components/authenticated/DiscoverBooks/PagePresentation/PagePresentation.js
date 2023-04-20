086b38c0feee7a1a9b35ff99e18251839dc6fd7e
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-20 12:27:28 -0300

    setted main page schema

--- src/components/authenticated/DiscoverBooks/PagePresentation/PagePresentation.js
+++ src/components/authenticated/DiscoverBooks/PagePresentation/PagePresentation.js
@@ -4,6 +4,7 @@ function PagePresentation() {
   return (
     <div className="page-presentation">
       <h1>Open Book</h1>
+      <p>Find your favorites books and keep track of them</p>
     </div>
   );
 }

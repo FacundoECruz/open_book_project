841b94656f510a5b96073f74a25b6f026299428c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-21 11:19:16 -0300

    added modal header

--- src/components/unauthenticated/Modal/Modal.js
+++ src/components/unauthenticated/Modal/Modal.js
@@ -1,14 +1,18 @@
 import LoginForm from "../LoginForm/LoginForm";
-import "./Modal.css"
+import "./Modal.css";
 
-function Modal({btnText, close, onSubmit}) {
-  return ( 
+function Modal({ btnText, close, onSubmit }) {
+  return (
     <div className="modal-container">
-      <h2>{btnText}</h2>
-      <button onClick={close} className="close-modal-btn">X</button>
-      <LoginForm btnText={btnText} onSubmit={onSubmit}/>
+      <div className="modal-header">
+        <h2>{btnText}</h2>
+        <button onClick={close} className="close-modal-btn">
+          X
+        </button>
+      </div>
+      <LoginForm btnText={btnText} onSubmit={onSubmit} />
     </div>
-   );
+  );
 }
 
 export default Modal;

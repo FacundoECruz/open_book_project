fd9ecc3ecdcd1fa77e0e1f801379072269617794
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-21 11:33:35 -0300

    added error msg if empty fields

--- src/components/unauthenticated/LoginForm/LoginForm.js
+++ src/components/unauthenticated/LoginForm/LoginForm.js
@@ -1,11 +1,19 @@
 import "./LoginForm.css"
+import { useState } from "react"
 
 function LoginForm({btnText, onSubmit}) {
+
+  const [error, setError] = useState(null)
 
   function handleSubmit(e) {
     e.preventDefault()
     const username = e.target.elements.username.value
     const password = e.target.elements.password.value
+
+    if(username || password === "") {
+      setError("Please complete all fields")
+    }
+
     const formData = {username: username, password: password}
     onSubmit(formData)
   }
@@ -21,6 +29,7 @@ function LoginForm({btnText, onSubmit}) {
           <input type="password" id="password" />
         </div>
         <button type="submit">{btnText}</button>
+        {error ? <p>{error}</p> : null}
       </form>
    );
 }

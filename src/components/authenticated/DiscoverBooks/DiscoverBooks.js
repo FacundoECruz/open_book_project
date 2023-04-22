89aeeb6a028bd307701353dfc7e57c2ac0f2664b
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-22 08:40:10 -0300

    added query, queried, status state to discover

--- src/components/authenticated/DiscoverBooks/DiscoverBooks.js
+++ src/components/authenticated/DiscoverBooks/DiscoverBooks.js
@@ -1,16 +1,29 @@
-import { useState, useRef } from "react";
+import { useState, useEffect } from "react";
 import client from "../../../utils/api-client";
 import SearchBar from "./SearchBar/SearchBar";
 import BookResult from "../BookResult/BookResult";
 import "./DiscoverBooks.css";
 import { books } from "../../../utils/fake-search-results";
 
-function DiscoverBooks({user, logout}) {
+function DiscoverBooks({ user, logout }) {
   const [data, setData] = useState(books);
+  const [query, setQuery] = useState(null);
+  const [queried, setQueried] = useState(false);
+  const [status, setStatus] = useState("idle");
 
   const searchBook = (formData) => {
-    client(formData).then((data) => setData(data));
+    setQuery(formData);
+    setQueried(true);
   };
+
+  // useEffect(() => {
+  //   if (!queried) {
+  //     return;
+  //   }
+  //   setStatus("loading");
+  //   client(encodeURIComponent(query)).then((data) => setData(data.JSON()));
+  //   setStatus("success");
+  // }, [query, queried]);
 
   return (
     <>
@@ -18,7 +31,7 @@ function DiscoverBooks({user, logout}) {
         <div className="page-presentation">
           <h1>Find your favorites books and keep track of them.</h1>
         </div>
-          <SearchBar onSubmit={searchBook} user={user} logout={logout}/>
+        <SearchBar onSubmit={searchBook} user={user} logout={logout} />
       </div>
       <div className="results">
         {data

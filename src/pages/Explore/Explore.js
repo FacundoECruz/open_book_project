d09831aa66edda9ed89241429ed00d96cb6794a6
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-30 08:51:57 -0300

    rendering search form in explore

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,10 +1,22 @@
 import BookResult from "../../components/ui/BookResult";
+import { useRef } from "react";
 import useQuery from "../../hooks/useQuery";
-import "../../stylesheets/Explore.css"
+import FormInput from "../../components/form/FormInput";
+import "../../stylesheets/Explore.css";
 
 function Explore({ user, logout }) {
-  
-  const {data, status} = useQuery()
+  const { data, status, searchBook } = useQuery();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const query = e.target.elements.query.value;
+    if (query === "") {
+      return;
+    }
+    console.log(query);
+    searchBook(query)
+    e.target.elements.query.value = ""
+  }
 
   return (
     <>
@@ -12,12 +24,18 @@ function Explore({ user, logout }) {
         <div className="page-presentation">
           <h1>Find your favorites books and keep track of them.</h1>
         </div>
+        <form className="search-form-container" onSubmit={handleSubmit}>
+          <FormInput label="query" type="text" placeholder="Enter your book name"/>
+          {status === "loading" ? (
+            <p>Loading...</p>
+          ) : (
+            <button>
+              <i className="fa-solid fa-magnifying-glass" type="submit"></i>
+            </button>
+          )}
+        </form>
       </div>
-      <div className="results">
-        {data
-          ? <BookResult data={data} />
-          : null}
-      </div>
+      <div className="results">{data ? <BookResult data={data} /> : null}</div>
     </>
   );
 }
